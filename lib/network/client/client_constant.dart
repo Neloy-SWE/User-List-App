273d364048fdf30195e0ce2d7f5358af18c79db7
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-11-15 13:09:52 +0600

    client configuration added

---
+++ lib/network/client/client_constant.dart
@@ -0,0 +1,12 @@
+/* 
+Created by Neloy on 15 November, 2025.
+Email: taufiqneloy.swe@gmail.com
+*/
+
+class ClientConstant {
+  static const String baseUrl = "https://reqres.in/api/users";
+
+  // api header keys, values:
+static const String xApiKey = "x-api-key";
+static const String token = "reqres-free-v1";
+}
